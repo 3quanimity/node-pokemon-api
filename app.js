e9cac66e7b3d926f74392004593551f877415b07
Haythem <sinnner6@gmail.com>
2024-12-26 10:56:07 +0100

    first endpoint

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Express 👋🏼');
+});
+
+app.listen(port, () => {
+  console.log(`App started andlistening on 👉🏼 http://localhost:${port}`);
+});
