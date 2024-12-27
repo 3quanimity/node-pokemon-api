0f88edfd4b3fec211a3b8671a023f89b5ad693fc
Haythem <sinnner6@gmail.com>
2024-12-27 11:07:15 +0100

    playing with endpoints

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+let pokemons = require('./mock-pokemons');
 
 const app = express();
 const port = 3000;
 
+// returns a string saying hello
 app.get('/', (req, res) => {
   res.send('Hello Express 👋🏼');
 });
 
+// returns the pokemon that matches the requested id
+app.get('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemon = pokemons.find(pokemon => pokemon.id === id);
+  res.json(pokemon);
+});
+
+// returns a string saying how many pokemons there are in the database
+app.get('/api/pokemons', (req, res) => {
+  res.send(`There are ${pokemons.length} pokemons in the database`);
+});
+
+// start the server and listen on the given port
 app.listen(port, () => {
-  console.log(`App started andlistening on 👉🏼 http://localhost:${port}`);
+  console.log(`App started and listening on 👉🏼 http://localhost:${port}`);
 });
---
+++ mock-pokemons.js
@@ -0,0 +1,88 @@
+const pokemons = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    type: ["Grass", "Poison"],
+    hp: 45,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+  },
+  {
+    id: 2,
+    name: "Ivysaur",
+    type: ["Grass", "Poison"],
+    hp: 60,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg"
+  },
+  {
+    id: 3,
+    name: "Venusaur",
+    type: ["Grass", "Poison"],
+    hp: 80,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"
+  },
+  {
+    id: 4,
+    name: "Charmander",
+    type: ["Fire"],
+    hp: 39,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"
+  },
+  {
+    id: 5,
+    name: "Charmeleon",
+    type: ["Fire"],
+    hp: 58,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg"
+  },
+  {
+    id: 6,
+    name: "Charizard",
+    type: ["Fire", "Flying"],
+    hp: 78,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg"
+  },
+  {
+    id: 7,
+    name: "Squirtle",
+    type: ["Water"],
+    hp: 44,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg"
+  },
+  {
+    id: 8,
+    name: "Wartortle",
+    type: ["Water"],
+    hp: 59,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/8.svg"
+  },
+  {
+    id: 9,
+    name: "Blastoise",
+    type: ["Water"],
+    hp: 79,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg"
+  },
+  {
+    id: 10,
+    name: "Caterpie",
+    type: ["Bug"],
+    hp: 45,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/10.svg"
+  },
+  {
+    id: 11,
+    name: "Metapod",
+    type: ["Bug"],
+    hp: 50,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg"
+  },
+  {
+    id: 12,
+    name: "Butterfree",
+    type: ["Bug", "Flying"],
+    hp: 60,
+    picture: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/12.svg"
+  }
+];
+
+module.exports = pokemons;
